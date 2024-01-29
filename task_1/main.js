b47c7ffd8952112bd7fb4026b57c4ff21b8494b0
saharassss <sasharasheva@gmail.com>
2024-01-29 11:47:11 +0300

    Создали файл и подключили js

---
+++ task_1/main.js
@@ -0,0 +1,10 @@
+
+let tempa = 20;
+
+if (tempa >= 30) {
+    console.log('Вы можете надеть легкую одежду');
+} else if (tempa >= 20) {
+    console.log('Нужно надеть теплую одежду');
+} else if (tempa <= 10) {
+    console.log('Сегодня холодно и нужно надеть теплую одежду')
+}
